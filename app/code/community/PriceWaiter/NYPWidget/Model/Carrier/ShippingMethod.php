8e69f8e3274fde4ee48167959cfc6dacd6ff59ea
Magento Extension <extensions@pricewaiter.com>
2013-09-11 16:52:39 -0400

    Fixed bug that allowed shipping method to display incorrectly.

--- app/code/community/PriceWaiter/NYPWidget/Model/Carrier/ShippingMethod.php
+++ app/code/community/PriceWaiter/NYPWidget/Model/Carrier/ShippingMethod.php
@@ -16,29 +16,19 @@
  *
  */
 class PriceWaiter_NYPWidget_Model_Carrier_ShippingMethod
-    extends Mage_Shipping_Model_Carrier_Abstract
-    implements Mage_Shipping_Model_Carrier_Interface
+	extends Mage_Shipping_Model_Carrier_Abstract
+	implements Mage_Shipping_Model_Carrier_Interface
 {
-    protected $_code = 'nypwidget';
-    protected $_isFixed = true;
+	protected $_code = 'nypwidget';
+	protected $_isFixed = true;
 
-    public function collectRates(Mage_Shipping_Model_Rate_Request $request)
-    {
-        $result = Mage::getModel('shipping/rate_result');
-        $method = Mage::getModel('shipping/rate_result_method');
-        $method->setCarrier($this->_code);
-        $method->setCarrierTitle($this->getConfigData('title'));
-        $method->setMethod($this->_code);
-        $method->setMethodTitle($this->getConfigData('name'));
-        $shippingPrice = '0.00';
-        $method->setPrice($shippingPrice);
-        $method->setCost($shippingPrice);
-        $result->append($method);
-        return $result;
-    }
+	public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+	{
+		return;
+	}
 
-    public function getAllowedMethods()
-    {
-        return array($this->_code => $this->getConfigData('name'));
-    }
+	public function getAllowedMethods()
+	{
+		return;
+	}
 }
