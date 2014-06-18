403af618669df9dc7774bb94f25fa40e066f3f7f
Magento Extension <extensions@pricewaiter.com>
2014-06-18 14:52:52 -0400

    Updated copyright notice.
    `find . -type f -not -wholename '*.git*' -exec sed -i '' \
    's/Copyright 2013/Copyright 2013-2014/' {} \;`

--- app/code/community/PriceWaiter/NYPWidget/Block/Category.php
+++ app/code/community/PriceWaiter/NYPWidget/Block/Category.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2013 Price Waiter, LLC
+ * Copyright 2013-2014 Price Waiter, LLC
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
