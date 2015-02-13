6412af14ca9a42797c61b76725e01f17f1947365
Magento Extension <extensions@pricewaiter.com>
2015-02-13 14:37:06 -0500

    Install magento sample data on travis

--- script/install-magento.sh
+++ script/install-magento.sh
@@ -4,6 +4,14 @@ set -ex
 # Create database for test store
 mysql -e 'create database teststore;'
 
+# Configure php and n98-magerun
+phpenv config-add "$TRAVIS_BUILD_DIR/travis/travis.php.ini"
+cp "$TRAVIS_BUILD_DIR/travis/.n98-magerun.yml" "$HOME/."
+
+# Create install directory
+mkdir -p "$HOME/build"
+cd "$HOME/build"
+
 # Install magento
 mage install \
     --baseUrl="http://magento.localdomain/" \
@@ -11,7 +19,7 @@ mage install \
     --dbName="teststore" \
     --dbPass="" \
     --dbUser="root" \
-    --installationFolder=$HOME/build \
-    --installSampleData=no \
+    --installationFolder=. \
+    --installSampleData=yes \
     --magentoVersionByName="magento-ce-1.9.0.1" \
     --useDefaultConfigParams=yes
