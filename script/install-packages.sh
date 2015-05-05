3b426d53431ea4fc8316a57aeac7f7b4270893f6
Magento Extension <extensions@pricewaiter.com>
2015-05-05 14:52:04 -0400

    Turn off strict-ssl in nodejs

--- script/install-packages.sh
+++ script/install-packages.sh
@@ -3,4 +3,7 @@ set -ex
 
 sudo apt-get -qq update
 sudo apt-get -qq install libxml2 libxml2-utils nginx-full nodejs npm
+
+# Install coffe-script (have to workaround this error: https://github.com/npm/npm/issues/2245)
+sudo npm config set strict-ssl false
 sudo npm install -g coffee-script
