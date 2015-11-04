10e35bc45a37dda568571d0532c089d136d9207d
Mike Estes <mikejestes@gmail.com>
2015-11-04 12:13:34 -0800

    try using a mirror.

--- script/install-magento.sh
+++ script/install-magento.sh
@@ -20,7 +20,7 @@ mage install \
     --dbUser="root" \
     --installationFolder=. \
     --installSampleData=yes \
-    --magentoVersionByName="magento-ce-1.9.1.0" \
+    --magentoVersionByName="magento-mirror-1.9.2.2" \
     --useDefaultConfigParams=yes
 
 # Testing
