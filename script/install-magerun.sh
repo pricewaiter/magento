ffefa9eff1cf1bcacb0632fa54015504947922fa
Mike Estes <mikejestes@gmail.com>
2015-11-04 11:16:44 -0800

    Adjust url of mage download.

--- script/install-magerun.sh
+++ script/install-magerun.sh
@@ -2,5 +2,5 @@
 set -ex
 
 # Download and install n98-magerun
-sudo wget -O /usr/local/bin/mage https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
+sudo wget -O /usr/local/bin/mage http://files.magerun.net/n98-magerun-latest.phar
 sudo chmod a+x /usr/local/bin/mage
